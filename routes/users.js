d5111149e0865aede96a885aadc95935185bed7c
Nicolás Javier Billán Danylyszyn <billan_njd@hotmail.com>
2022-10-16 20:15:59 -0300

    - token response changed to json

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ router.get('/:email', async function (req, res) {
 router.post('/login', async function (req, res) {
   return Keyrock.getToken(req.body.email, req.body.password)
     .then((result) => createToken(result, req.body.email))
-    .then((result) => res.send(result))
+    .then((result) => res.json(result))
     .catch((error) => res.status(error.code).send(error.message));
 });
 
